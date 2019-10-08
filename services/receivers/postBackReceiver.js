e14df75aa77f2854c8685eeb8ec6ce2de51101a0
Mohamed Farid <mohamedamal680@gmail.com>
2019-10-08 21:45:49 +0200

    add good and bad keywards config

--- services/receivers/postBackReceiver.js
+++ services/receivers/postBackReceiver.js
@@ -6,7 +6,9 @@ class PostBackReceiver {
   static router(user, event) {
     const { postback } = event;
     const { payload } = postback;
-    let parsed = BotUtils.parsePayload(payload);
+    const parsed = BotUtils.parsePayload(payload);
+    console.log('parsed.action: ', parsed.action);
+
     if (!parsed) throw new Error('received non JSON payload');
     // else if (parsed.action === configConstants.CONFIGURE_SARAH) {
     //   const actions = ['posts', 'comments', 'replies'];
@@ -16,6 +18,14 @@ class PostBackReceiver {
     // }
     else if (parsed.action === configConstants.CONFIGURE_SARAH) {
       PostBackReceiver.receivedStartConfiguration(user);
+    }
+
+    else if (parsed.action === configConstants.GOOD_KEYWARDS) {
+      PostBackReceiver.receivedGoodKeywards(user);
+    }
+
+    else if (parsed.action === configConstants.BAD_KEYWARDS) {
+      PostBackReceiver.receivedBadKeywards(user);
     }
 
     else PostBackReceiver.receivedGreetingMessage(user, event);
@@ -28,6 +38,24 @@ class PostBackReceiver {
     await user.save();
     const message = 'can you enter percentage of confidence ?'
     senderService.sendConfidence(user, message);
+  }
+
+  static async receivedGoodKeywards(user) {
+    const session = user.session || {};
+    session.step = configConstants.GOOD_KEYWARDS;
+    user.markModified('session');
+    await user.save();
+    const message = 'what do you want ?';
+    senderService.sendKeywardsInstruction(user, message, configConstants.GOOD_KEYWARDS);
+  }
+
+  static async receivedBadKeywards(user) {
+    const session = user.session || {};
+    session.step = configConstants.BAD_KEYWARDS;
+    user.markModified('session');
+    await user.save();
+    const message = 'what do you want ?';
+    senderService.sendKeywardsInstruction(user, message, configConstants.BAD_KEYWARDS);
   }
 
   static receivedGreetingMessage(user, event) {
