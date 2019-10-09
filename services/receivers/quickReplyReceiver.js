719074ca7f932e77dd32b14c255bf770e4dea683
Ahmed Mashhour <mashhour04@gmail.com>
2019-10-09 03:59:13 +0200

    getting keywords the beginning of development

--- services/receivers/quickReplyReceiver.js
+++ services/receivers/quickReplyReceiver.js
@@ -4,33 +4,33 @@ const configConstants = require('../constants/configConstants');
 
 class QuickReplyReceiver {
   static router(user, event) {
-    let payload = event.message.quick_reply.payload;
-    let parsed = BotUtils.parsePayload(payload);
+    const { payload } = event.message.quick_reply;
+    const parsed = BotUtils.parsePayload(payload);
     if (!parsed) {
       throw new Error('received non JSON payload');
-    }
-
-    else if (parsed.action === configConstants.ADD_GOOD_KEYWORDS) {
+    } else if (parsed.action === configConstants.ADD_GOOD_KEYWORDS) {
       QuickReplyReceiver.receivedAddGoodKeywords(user);
-    }
-
-    else if (parsed.action === configConstants.ADD_BAD_KEYWORDS) {
+    } else if (parsed.action === configConstants.ADD_BAD_KEYWORDS) {
       QuickReplyReceiver.receivedAddBadKeywords(user);
+    } else if (parsed.action === configConstants.LIST_GOOD_KEYWORDS) {
+      QuickReplyReceiver.receivedListKeyWords(user, 'good');
+    } else if (parsed.action === configConstants.LIST_BAD_KEYWORDS) {
+      QuickReplyReceiver.receivedListKeyWords(user, 'bad');
+    } else {
+      // Unhandled_Quick_Reply
     }
-
-
-    else QuickReplyReceiver.receivedGreetingMessage(user, event);
   }
 
   static receivedGreetingMessage(user, event) {
     senderService.sendGreetingMessage(user);
   }
+
   static async receivedPostType(user, event) {
     const session = user.session || {};
     session.step = configConstants.PERCENTAGE_OF_CONFIDENCE;
     user.markModified('session');
     await user.save();
-    const message = 'can you enter PERCENTAGE OF CONFIDENCE ?'
+    const message = 'can you enter PERCENTAGE OF CONFIDENCE ?';
     senderService.sendConfidence(user, message);
   }
 
@@ -50,6 +50,10 @@ class QuickReplyReceiver {
     await user.save();
     const message = 'Please Enter Your Good Keywords Separated By Commas "," !';
     senderService.sendBadKeywordsInstruction(user, message);
+  }
+
+  static async receivedListKeyWords(user, type) {
+    senderService.sendKeywords(user, type);
   }
 }
 
